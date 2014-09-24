4389654399e710dd1d18d558caa49e4b99b35edc
mathieuvandenmooter <mathieuvandenmooter@192.168.0.156>
2014-09-24 22:07:59 +0200

    test

---
+++ ServerSetup/Server.js
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var http = require("http");
+var url = require("url");
+
+function start(route) {
+  function onRequest(request, response) {
+
+		var pathname = url.parse(request.url).pathname,
+			postData = '';
+
+		request.setEncoding('utf8');
+
+		request.addListener('data', function (postDataChunk) {
+			postData += postDataChunk;
+		});
+
+		request.addListener('end', function () {
+			route(request,pathname, response);
+		});
+	}
+
+	http.createServer(onRequest).listen(8080);
+}
+
+exports.start = start;
